716a01267f5bb03b771e5904f98c4fc7cb78c7f3
avd <aravind19112003@gmail.com>
2023-08-11 18:21:19 +0530

    11/08/23

---
+++ 15. 3Sum.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+"https://leetcode.com/problems/3sum/solutions/3523898/beats-99-48-44-145-top-interview-question/"
+
+'''
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+'''
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        negative = defaultdict(int)
+        positive = defaultdict(int)
+        zeros = 0
+        for num in nums:
+            if num < 0:
+                negative[num] += 1
+            elif num > 0:
+                positive[num] += 1
+            else:
+                zeros += 1
+
+        result = []
+        if zeros:
+            for n in negative:
+                if -n in positive:
+                    result.append((0, n, -n))
+            if zeros > 2:
+                result.append((0, 0, 0))
+
+        for set1, set2 in ((negative, positive), (positive, negative)):
+            set1Items = list(set1.items())
+            for i, (j, k) in enumerate(set1Items):
+                for j2, k2 in set1Items[i:]:
+                    if j != j2 or (j == j2 and k > 1):
+                        if -j-j2 in set2:
+                            result.append((j, j2, -j-j2))
+        return result
+
+# 428ms runtime 20.28mb memory
