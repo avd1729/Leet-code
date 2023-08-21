22e5cffdd6f5f9eea39cda16270986561206703a
avd <aravind19112003@gmail.com>
2023-08-21 22:41:36 +0530

    21/08/23

---
+++ 205. Isomorphic Strings.py
@@ -0,0 +1,53 @@
+'''
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
+'''
+
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        map1 = []
+        map2 = []
+        for idx in s:
+            map1.append(s.index(idx))
+        for idx in t:
+            map2.append(t.index(idx))
+        if map1 == map2:
+            return True
+        return False
+
+
+'''
+Runtime
+35ms
+Beats 37.36%of users with Python
+
+Memory
+14.01MB
+Beats 80.83%of users with Python
+'''
