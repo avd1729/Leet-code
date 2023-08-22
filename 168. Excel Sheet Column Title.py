e085af40e8ca8583bf499818f635600cfbf59a90
avd <aravind19112003@gmail.com>
2023-08-22 21:13:08 +0530

    22/08/23

---
+++ 168. Excel Sheet Column Title.py
@@ -0,0 +1,53 @@
+'''
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnNumber = 1
+Output: "A"
+Example 2:
+
+Input: columnNumber = 28
+Output: "AB"
+Example 3:
+
+Input: columnNumber = 701
+Output: "ZY"
+ 
+
+Constraints:
+
+1 <= columnNumber <= 231 - 1
+'''
+
+
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        out = ""
+        while columnNumber > 0:
+            out = chr(ord('A') + (columnNumber - 1) % 26) + out
+            columnNumber = (columnNumber - 1) // 26
+        return out
+
+
+'''
+Runtime
+16ms
+Beats 50.90%of users with Python
+
+Memory
+13.35MB
+Beats 24.19%of users with Python
+'''
