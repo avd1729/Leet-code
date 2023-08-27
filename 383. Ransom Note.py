703eabefdd6a4f5177fd0d0fb2611728c7354a3e
avd <aravind19112003@gmail.com>
2023-08-27 13:06:48 +0530

    27/08/23

---
+++ 383. Ransom Note.py
@@ -0,0 +1,60 @@
+'''
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ 
+
+Constraints:
+
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters.
+'''
+
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        # Create a dictionary to store character counts
+        dictionary = {}
+
+        # Iterate through the magazine and count characters
+        for char in magazine:
+            if char not in dictionary:
+                dictionary[char] = 1
+            else:
+                dictionary[char] += 1
+
+        # Iterate through the ransom note and check character counts
+        for char in ransomNote:
+            if char in dictionary and dictionary[char] > 0:
+                dictionary[char] -= 1
+            else:
+                return False
+
+        return True
+
+
+'''
+Runtime
+Details
+76ms
+Beats 38.37%of users with Python3
+Memory
+Details
+16.50MB
+Beats 91.55%of users with Python3
+'''
