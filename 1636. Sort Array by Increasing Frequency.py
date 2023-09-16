8d7395c2844297e0cab9b6203913fa85c958d277
avd <aravind19112003@gmail.com>
2023-09-16 20:50:19 +0530

    16/09/23

---
+++ 1636. Sort Array by Increasing Frequency.py
@@ -0,0 +1,46 @@
+'''
+Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+
+Return the sorted array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,2,2,3]
+Output: [3,1,1,2,2,2]
+Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
+Example 2:
+
+Input: nums = [2,3,1,3,2]
+Output: [1,3,3,2,2]
+Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
+Example 3:
+
+Input: nums = [-1,1,-6,4,5,-6,1,4,1]
+Output: [5,-1,4,4,-6,-6,1,1,1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100
+'''
+
+
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        counts_dict = Counter(nums)
+        return sorted(nums, key=lambda x: (counts_dict[x], -x))
+
+
+'''
+Runtime
+Details
+54ms
+Beats 67.62%of users with Python3
+Memory
+Details
+16.30MB
+Beats 90.86%of users with Python3
+'''
