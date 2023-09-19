4100d66b701bcdf89ecbe24e6b88497f76af942a
avd <aravind19112003@gmail.com>
2023-09-19 20:45:55 +0530

    19/09/23

---
+++ 1812. Determine Color of a Chessboard Square.py
@@ -0,0 +1,50 @@
+'''
+You are given coordinates, a string that represents the coordinates of a square of the chessboard. Below is a chessboard for your reference.
+
+
+
+Return true if the square is white, and false if the square is black.
+
+The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first, and the number second.
+
+ 
+
+Example 1:
+
+Input: coordinates = "a1"
+Output: false
+Explanation: From the chessboard above, the square with coordinates "a1" is black, so return false.
+Example 2:
+
+Input: coordinates = "h3"
+Output: true
+Explanation: From the chessboard above, the square with coordinates "h3" is white, so return true.
+Example 3:
+
+Input: coordinates = "c7"
+Output: false
+ 
+
+Constraints:
+
+coordinates.length == 2
+'a' <= coordinates[0] <= 'h'
+'1' <= coordinates[1] <= '8'
+'''
+
+
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        return 1 & (ord(coordinates[0]) + int(coordinates[1]))
+
+
+'''
+Runtime
+Details
+29ms
+Beats 96.11%of users with Python3
+Memory
+Details
+16.18MB
+Beats 86.49%of users with Python3
+'''
