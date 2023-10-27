6a262056541b229bc3aa14888c129b94496520a4
avd <aravind19112003@gmail.com>
2023-10-27 22:33:45 +0530

    27/10/23

---
+++ 456. 132 Pattern.py
@@ -0,0 +1,55 @@
+'''
+Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+
+Return true if there is a 132 pattern in nums, otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: There is no 132 pattern in the sequence.
+Example 2:
+
+Input: nums = [3,1,4,2]
+Output: true
+Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
+Example 3:
+
+Input: nums = [-1,3,2,0]
+Output: true
+Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 2 * 105
+-109 <= nums[i] <= 109
+'''
+
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack, third = [], float('-inf')
+
+        for num in reversed(nums):
+            if num < third:
+                return True
+            while stack and stack[-1] < num:
+                third = stack.pop()
+            stack.append(num)
+        return False
+
+
+'''
+Runtime
+Details
+548ms
+Beats 12.28%of users with Python3
+Memory
+Details
+36.54MB
+Beats 37.56%of users with Python3
+'''
