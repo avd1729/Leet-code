f0ddaa4b01230992c4b4931d5fbb4396f9f796b6
avd <aravind19112003@gmail.com>
2023-10-28 19:48:31 +0530

    28/10/23

---
+++ 40. Combination Sum II.py
@@ -0,0 +1,66 @@
+'''
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+
+ 
+
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[
+[1,2,2],
+[5]
+]
+ 
+
+Constraints:
+
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30
+'''
+
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+
+        def dfs(idx, path, cur):
+            if cur > target:
+                return
+            if cur == target:
+                res.append(path)
+                return
+            for i in range(idx, len(candidates)):
+                if i > idx and candidates[i] == candidates[i-1]:
+                    continue
+                dfs(i+1, path+[candidates[i]], cur+candidates[i])
+        dfs(0, [], 0)
+        return res
+
+
+'''
+Runtime
+Details
+76ms
+Beats 55.00%of users with Python3
+Memory
+Details
+16.33MB
+Beats 60.33%of users with Python3
+'''
